ca2ee8dc02ef00a9c5b50cab4bc92c79128930b6
Philip Vieira <philip@vallin.se>
2011-12-30 21:37:59 +0100

    Fixed link and fixed link spec
    Signed-off-by: Philip Vieira <philip@vallin.se>

--- lib/uriversal/link.rb
+++ lib/uriversal/link.rb
@@ -2,15 +2,12 @@ module Uriversal
 
   class Link
     
-    STATUSES = { ok: ['200'], broken: ['404','500','303'] }
-    CATEGORIES = [:image,:document,:article,:video,:other]
-    
     attr_accessor :raw, :protocol, :domain, :port, :path, :file_type, :query
-    attr_accessor :category, :title, :description, :status
+    attr_accessor :title, :description, :status
     
     def initialize(url,options={})
       if url =~ /^((ftp|https?):\/\/)?([a-z\d]+([\-\.][a-z\d]+)*\.[a-z]{2,6})((:(\d{1,5}))?((\/[\w\'\"\,\-\/\:\&\=\#\%\+\(\)]*)(\.([a-z]{0,4}))?(\?.*)?)?)?$/
-        self.raw       = $0
+        self.raw = url
         self.protocol  = $2     || 'http'
         self.domain    = $3     || ''
         self.port      = $7
@@ -28,7 +25,8 @@ module Uriversal
     end
     
     def load_data!
-      { status: '404', title: '', description: '', metadata: {} }
+      Uriversal::Registry.match(self)
+      true
     end
     
   end
--- spec/uriversal/link_spec.rb
+++ spec/uriversal/link_spec.rb
@@ -2,51 +2,47 @@ require 'spec_helper'
 
 describe Uriversal::Link do
   
-  before(:each) do
-    # Sets a default instance
-    @invalid_link = Uriversal.parse('http://google.com/not_found')
-    @valid_link = Uriversal.parse('http://google.com/')
-  end
-
   describe '.new' do
-    it 'should return self as instance' do
-      @valid_link.class.should == Uriversal::Link
-      @invalid_link.class.should == Uriversal::Link
+    
+    before do
+      @link = Uriversal::Link.new('http://google.com:3000/path.png?q="hello"')
     end
+    
+    it 'should return self as instance' do
+      @link.class.should == Uriversal::Link
+    end
+    
+    it 'should have specific values' do
+      @link.raw.should == 'http://google.com:3000/path.png?q="hello"'
+      @link.protocol.should == 'http'
+      @link.domain.should == 'google.com'
+      @link.port.should == '3000'
+      @link.path.should == '/path'
+      @link.file_type.should == 'png'
+      @link.query.should == '?q="hello"'
+    end
+    
   end
   
   describe '.load_data!' do
-    it 'should return a hash' do
-      @valid_link.load_data!.is_a?(Hash).should == true
-      @invalid_link.load_data!.is_a?(Hash).should == true
+    
+    it 'should return true if request was successful' do
+      @link = Uriversal::Link.new('http://google.com/')
+      @link.load_data!.should == true
     end
     
-    it 'valid links should return a status as a string' do
-      @valid_link.load_data![:status].class.should == String
-    end
-    
-    it 'valid links should return a title as a string' do
-      @valid_link.load_data![:title].class.should == String
-    end
-    
-    it 'valid links should return a description as a string' do
-      @valid_link.load_data![:title].class.should == String
-    end
-    
-    it 'valid links should return a hash of metadata' do
-      @valid_link.load_data![:metadata].class.should == Hash
-    end
   end
   
   describe '.is_file?' do
+    
     it 'should return true as file is set to .png' do
-      @valid_link.file_type = 'png'
-      @valid_link.is_file?.should == true
+      @link = Uriversal::Link.new('http://google.com/path/to/file.jpg')
+      @link.is_file?.should == true
     end
     
     it 'should return false as file is not set' do
-      @valid_link.file_type = ''
-      @valid_link.is_file?.should == false
+      @link = Uriversal::Link.new('http://google.com/path/to/file')
+      @link.is_file?.should == false
     end
     
   end
