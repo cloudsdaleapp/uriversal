708ce6f9674a83fe8763f5f1c9930d3f98a79e4e
Philip Vieira <philip@vallin.se>
2011-12-31 17:38:49 +0100

    Fix on url parsing
    Signed-off-by: Philip Vieira <philip@vallin.se>

--- lib/uriversal/url.rb
+++ lib/uriversal/url.rb
@@ -5,18 +5,15 @@ module Uriversal
     attr_reader :raw, :protocol, :domain, :port, :path, :file_type, :query, :uri
     
     def initialize(url,options={})
-      unless (url =~ URI::regexp).nil?
-        @uri        = Addressable::URI.heuristic_parse(url)
-        @raw        = @uri.to_s
-        @protocol   = @uri.scheme
-        @domain     = @uri.host
-        @port       = @uri.port
-        @path       = @uri.path.chomp(File.extname(@uri.path))
-        @file_type  = File.extname(@uri.path).sub(/\./i,"")
-        @query      = @uri.query
-      else
-        raise ArgumentError, "invalid url"
-      end
+      @uri        = Addressable::URI.heuristic_parse(url)
+      @raw        = @uri.to_s
+      @protocol   = @uri.scheme
+      @domain     = @uri.host
+      @port       = @uri.port
+      @path       = @uri.path.chomp(File.extname(@uri.path))
+      @file_type  = File.extname(@uri.path).sub(/\./i,"")
+      @query      = @uri.query
+      # raise ArgumentError, "invalid url"
     end
     
     # Determains if linked url is a file
